b1db573f565d6fa1b0a87e55dae95d942c0d23c1
SanderDale <57624156+SanderDale@users.noreply.github.com>
2021-06-06 15:39:29 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd7d4127a060325c7f103f7e65a25882'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5ae218a-ae38-461c-859b-1d6fc7f6bff8'
+};
